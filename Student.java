30bb5fc76e047d19c9bb1a41509c7f413ea478fc
SafialIslam302 <safialislam302@gmail.com>
2016-10-15 13:09:17 +0600

    ayon

---
+++ Student.java
@@ -0,0 +1,95 @@
+
+/**
+ * This is a class to define the necessary attributes and methods to conceptualize a "Student"
+ * The spepcific tasks are:
+ * 1. Instiate 
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Student
+{
+    // Declare the important attributes of a student. For example:
+    //1.Id
+    //2. Name
+    //3. Department
+    //4. University
+    //5. GPAs in various terms (Multidimensional arrays)
+    //6. subjects for Current terms
+    //7. Credits and grades of Current Terms (Multidimmentional Array)
+   
+    
+
+    /**
+     * Define a constructor that initilize the default properties of the Student
+     */
+    public Student()
+    {
+        // initialise   variables with defult values
+        
+    }
+    /**
+     * Define a method to print the students details.
+     */
+    
+    public void studentDetailsById(int Id)
+    {
+        //write your code here
+    }
+    
+    /**
+     * Define a method to update information of students by ID
+     * Use as many arguments as required.
+     */
+    public void updateStudentById(int id)
+    {
+        //Write your code here
+    }
+    
+    /**
+     * Define a method to compute the CGPA from the Given term GPA for a particular student.
+     * se as many arguments as required.
+     */
+    public double computeCGPAByID()
+    {
+        // Write your code here
+    }
+    
+    /**
+     * Define a method to compute the GPA from the given Credits and Grades of all the subjects
+     */
+    public double computeGPAById()
+    {
+        //write yor code here
+    }
+    
+    /**
+     * After performing required operations on each student, save the information to a file. Use File and PrintWriter Class. 
+     * use as many arguments as required.
+     * 
+     */
+    public void saveStudents()
+    {
+        //Write your code here
+    }
+    
+    /**
+     * Create a dummy files with infromations to describe the properties of a Student object and load the data files using Java File and Scanner class.
+     * use as many arguments as required.
+     */
+    public void loadStudents()
+    {
+        //Write your code here
+        
+    }
+    
+    /**
+     * The tasks to be carried out here:
+     * 1. Create an Arrays of students using Student Class. Initlize them and perfom all the above defined operation to evualuate your code.
+     * use as many arguments as required.
+     */
+    public static void main(String[] args)
+    {
+        //Write your main function to execute call defined methods
+    }
+}
